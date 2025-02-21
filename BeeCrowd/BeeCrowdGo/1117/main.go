ce37fcafc7d72f935d977ec498eea445be59b010
napolisiqueira <imfelipenapoli@gmail.com>
2025-02-21 20:53:49 -0300

    Exercicio 1117 do BeeCrowd em Go.

---
+++ BeeCrowd/BeeCrowdGo/1117/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 float64 = -1
+	var num2 float64 = -1
+
+	for 0 > num1 || num1 > 10 {
+		fmt.Scanln(&num1)
+
+		if 0.0 >= num1 || num1 > 10.0 {
+			fmt.Println("valor invalido")
+			num1 = -1
+		}
+	}
+
+	for 0 > num2 || num2 > 10 {
+		fmt.Scanln(&num2)
+
+		if 0.0 >= num2 || num2 > 10.0 {
+			fmt.Println("valor invalido")
+			num2 = -1
+		}
+	}
+	var media float64 = (num1 + num2) / 2
+	fmt.Printf("media = %f.2f\n", media)
+
+}
